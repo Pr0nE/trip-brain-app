fbad8a97f578291afdcf844705c0b55a01979d0c
Pr0nE <moshi1376@yahoo.com>
2023-07-31 16:31:52 +0330

    Minor refactor

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
