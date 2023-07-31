877b0f7b073d7128bcfcadd9216aa7e7aa18d64f
Pr0nE <moshi1376@yahoo.com>
2023-07-31 16:56:58 +0330

    Move firebase initialization to app widget

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,22 +1,18 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:trip_brain_app/firebase_options.dart';
 import 'package:trip_brain_app/trip_brain_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
     testWidgets('Initial loading is visible', (tester) async {
       // Load app widget.
       await tester.pumpWidget(const TripBrainApp());
+      await tester.pump(Duration(seconds: 1));
 
       expect(find.text('Just a moment...'), findsOneWidget);
     });
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:trip_brain_app/firebase_options.dart';
 import 'package:trip_brain_app/trip_brain_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   if (kReleaseMode) {
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
