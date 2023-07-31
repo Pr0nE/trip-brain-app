5632a07e24a384f7d71beecb63fa8c0e848933e9
Pr0nE <moshi1376@yahoo.com>
2023-07-31 15:56:13 +0330

    Init integration test

---
+++ integration_test/app_test.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:trip_brain_app/firebase_options.dart';
+import 'package:trip_brain_app/trip_brain_app.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('Initial loading is visible', (tester) async {
+      // Load app widget.
+      await tester.pumpWidget(const TripBrainApp());
+
+      expect(find.text('Just a moment...'), findsOneWidget);
+    });
+  });
+}
